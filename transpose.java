5f3208301eee2edd3261d5224e16992a14f9e5c5
MichOchieng <MichOchieng@gmail.com>
2020-02-10 12:17:44 -0800

    Fixed transpose method

--- transpose.java
+++ transpose.java
@@ -5,15 +5,18 @@ public class transpose{
     private static int rows;
     private static int cols;
     private static int terms;
+    // Holds values and their positions
+    private static int a[][]; 
     public transpose(int rows, int cols){
         this.rows = rows;
         this.cols = cols;
         this.terms = 0;
+        a = new int[rows*cols][3];
     }
     
     // Result matrix    
-    private static int result[][] = new int[cols][rows];
-    private static int a[][] = new int[4][3];
+    private static int result[][];
+    
     
 
     public void insertVal(int x,int y,int z){ 
@@ -24,8 +27,9 @@ public class transpose{
             a[terms][1] = y;
             // Value 
             a[terms][2] = z;
+            terms++;
         }else{
-            System.out.println("The position " + x + ",0" + y + " doesn't exisit.");
+            System.out.println("The position (" + x + "," + y + ") doesn't exisit.");
         }
     }
 
@@ -42,33 +46,38 @@ public class transpose{
             Colsize[i] = 0;
         }
         for (int i = 0; i < terms; i++) {
-            Colsize[a[i][1]]++;
+            Colsize[a[i][1]]++;            
         }
     // The sum of rownext & colsize at that position
         int Rownext[] = new int [cols+1];
 
         Rownext[1] = 0;
 
-        for (int i = 2; i < terms; i++) {
-            Rownext[i] = Rownext[i-1] + Colsize[i-1];
+        for (int i = 2; i <= cols; i++) {
+            Rownext[i] = Rownext[i-1] + Colsize[i-1];            
         }
+
+        result = new int[cols][rows];
 
         for (int i = 0; i < terms; i++) {
             int j = Rownext[a[i][1]]++;
-
-            result[j][0] = a[i][1];
-            result[j][1] = a[i][0];
-            result[j][2] = a[i][2];
-
-            System.out.println(result[j][0]);
-            System.out.println(result[j][1]);
-            System.out.println(result[j][2]);
+            // Transposing row => col then col => row
+            result[a[i][1]][a[i][0]] = a[i][2];            
         }
     }
 
 
+    // private static void slowTranspose(){
+    //     for (int i = 0; i < result.length; i++) {
+    //         for (int j = 0; j < result.length; j++) {
+                
+    //         }            
+    //     }
+    // }
+
     // Prints out result matrix    
     private static void viewResultMtx(){
+        System.out.println("Transposed matrix:");
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result.length; j++) {
                 System.out.print(result[i][j] + " ");
